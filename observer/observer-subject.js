907ea0822087a093be1ffb1a5d862c26307f7915
Séra Bálint <sera.balint@e-vista.hu>
2016-03-28 13:56:00 +0200

    observer basics

--- observer/observer-subject.js
+++ observer/observer-subject.js
@@ -7,6 +7,7 @@ class ObserverSubject {
   
   /**
    * Add an element to the observer list
+   * @param object observer
    */
   add(observer) {
     this.observerList.push(observer);
@@ -21,6 +22,12 @@ class ObserverSubject {
     return this.observerList.length;
   }
   
+  /**
+   * Get observer at the specified index
+   * @param int index
+   * @returns object
+   * @throws Error
+   */
   get(index) {
     if (index < 0 || index > this.observerList.length - 1) {
       throw new Error('Too low or too high index');
@@ -28,8 +35,18 @@ class ObserverSubject {
     return this.observerList[index];
   }
   
+  
+  /**
+   * Get the index of an object
+   * @param obj (observerList element)
+   * @param startIndex
+   */
   indexOf(obj, startIndex) {
-    
+    for (var i in observerList) {
+      if (observerList[i] === obj) {
+        return i;
+      }
+    }
   }
   
   removeAt(index) {
