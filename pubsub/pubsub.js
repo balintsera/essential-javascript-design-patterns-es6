e888e3092bb0ae11bdd5825fdd9d0c17b40da3a5
Séra Bálint <sera.balint@e-vista.hu>
2016-03-29 18:17:40 +0200

    pubsub subscribe()

--- pubsub/pubsub.js
+++ pubsub/pubsub.js
@@ -1,8 +1,50 @@
 'use strict';
 
-class PubSub 
+class PubSub
 {
+  constructor() {
+    this.subscribers = [];
+    this.uniqId = -1;
+  }
   
+  
+  /**
+   * Adds a new subscriber to a topic
+   *
+   * @param topic (topic name)
+   * @param callb (callback funciton)
+   */
+  subscribe(topic, callb) {
+    this.addTopicIfNotExists(topic);
+    const subscriber = {
+      uniqId: toString(++this.uniqId),
+      callback: callb,
+    };
+    this.subscribers[topic].push(subscriber);
+  }
+  
+  
+  /**
+   * Get all subscribers by topic
+   *
+   * @param topic (description)
+   * @returns (description)
+   */
+  getSubscribersForTopic(topic) {
+    return this.subscribers[topic];
+  }
+  
+  
+  /**
+   * Add a new topic to the subscribers array
+   *
+   * @param topic (description)
+   */
+  addTopicIfNotExists(topic) {
+    if (!this.subscribers[topic]) {
+      this.subscribers[topic] = [];
+    }
+  }
 }
 
 module.exports = PubSub;
--- test/pubsub-test.js
+++ test/pubsub-test.js
@@ -12,7 +12,21 @@ const PubSub = require('../pubsub/pubsub');
 
 describe('Pubsub pattern - pubsub', function() {
   it('pubSub is an object', function() {
-    const subject = new PubSub();
-    expect(typeof subject).equals('object');
+    const pubSub = new PubSub();
+    expect(typeof pubSub).equals('object');
+  });
+  
+  it('adds a new topic to subscribers array', function() {
+    const pubSub = new PubSub();
+    pubSub.addTopicIfNotExists('test/topic');
+    expect(typeof pubSub.subscribers['test/topic']).not.equals('undefined');
+  });  
+    
+  it('subscribes to a topic', function() {
+    const pubSub = new PubSub();
+    pubSub.subscribe('user/mail', function(topics, payload) {
+      console.log('user/mail event happened');
+    });
+    expect(pubSub.getSubscribersForTopic('user/mail').length).equals(1);
   });
 });
